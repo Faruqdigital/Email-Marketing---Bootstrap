ab03353c9b923ec06fe0cd79bf2c2703de8a2d19
Faruq Hossain <faruqdigital01@gmail.com>
2023-04-12 00:32:57 +0600

    navbar added

--- main.js
+++ main.js
@@ -1,16 +1,49 @@
-const hambugerMenu = document.querySelector(".menu");
-const closeMenu = document.querySelector(".close-nav");
-const navMenu = document.querySelector("nav");
+window.addEventListener('DOMContentLoaded', event => {
 
-hambugerMenu.addEventListener("click", () => {
-  navMenu.classList.add("open-nav");
-  navMenu.classList.remove("back-nav");
+  // Navbar shrink function
+  var navbarShrink = function () {
+      const navbarCollapsible = document.body.querySelector('#mainNav');
+      if (!navbarCollapsible) {
+          return;
+      }
+      if (window.scrollY === 0) {
+          navbarCollapsible.classList.remove('navbar-shrink')
+      } else {
+          navbarCollapsible.classList.add('navbar-shrink')
+      }
+
+  };
+
+  // Shrink the navbar 
+  navbarShrink();
+
+  // Shrink the navbar when page is scrolled
+  document.addEventListener('scroll', navbarShrink);
+
+  // Activate Bootstrap scrollspy on the main nav element
+  const mainNav = document.body.querySelector('#mainNav');
+  if (mainNav) {
+      new bootstrap.ScrollSpy(document.body, {
+          target: '#mainNav',
+          rootMargin: '0px 0px -40%',
+      });
+  };
+
+  // Collapse responsive navbar when toggler is visible
+  const navbarToggler = document.body.querySelector('.navbar-toggler');
+  const responsiveNavItems = [].slice.call(
+      document.querySelectorAll('#navbarResponsive .nav-link')
+  );
+  responsiveNavItems.map(function (responsiveNavItem) {
+      responsiveNavItem.addEventListener('click', () => {
+          if (window.getComputedStyle(navbarToggler).display !== 'none') {
+              navbarToggler.click();
+          }
+      });
+  });
+
 });
 
-closeMenu.addEventListener("click", () => {
-  navMenu.classList.remove("open-nav");
-  navMenu.classList.add("back-nav");
-});
 
 function openPage(pageName, elmnt, color) {
   var i, tabcontent, tablinks;
